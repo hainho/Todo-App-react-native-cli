4a231f24d77a5426c77fb9020c6fb94244846854
Ha In Ho <hainho9704@gmail.com>
2020-08-24 18:02:59 +0900

    step1

---
+++ App.js
@@ -0,0 +1,42 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import Header from './Header';
+import Body from './Body';
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Hello world!!</Text>
+        <Header></Header>
+        <Body></Body>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    //비율 설정
+    flex: 1,
+
+    //container 내부의 아이템들의 방향
+    flexDirection: 'column',
+
+    //앱 전체와 상단과의 거리
+    paddingTop: 50,
+  },
+  title: {
+    fontWeight: '800',
+    fontSize: 30,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+});
